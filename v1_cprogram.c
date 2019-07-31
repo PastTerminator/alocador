80f66468512ae5b09204f002b2235640cf638935
Vicente <joao.vicenteft@gmail.com>
2019-07-31 10:43:33 -0400

    now with void function working

--- v1_cprogram.c
+++ v1_cprogram.c
@@ -1,1 +1,22 @@
-firstCprogram
+#include "stdio.h"
+#include "stdlib.h"
+
+void testFunction(void* variable) {
+	
+	int *value = (int*)variable;
+	
+	if (*value != 2) {
+		*value = 10;
+		variable = &value;
+	}
+
+}
+
+int main(int argc, char *argv[]) {
+
+	int value = 4;
+	
+	testFunction(&value);
+
+	printf("this is the new value %d\n", value);
+}
